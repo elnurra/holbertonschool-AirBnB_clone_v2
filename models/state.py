7b94891128447da20af42869c6d10df2c5badc99
elnurra <elnurra@code.edu.az>
2024-04-14 00:57:23 +0400

    feat: added task 8

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -15,3 +16,15 @@ class State(BaseModel, Base):
     name: Mapped[str] = mapped_column(String(128), nullable=False)
     cities: Mapped["City"] = relationship("City", backref="state",
                                           cascade="delete")
+
+    @property
+    def cities(self):
+        """
+            getter method, returns list of City objs from storage
+            linked to the current State
+        """
+        city_list = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
